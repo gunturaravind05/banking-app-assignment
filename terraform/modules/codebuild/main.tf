4c8e5cb019b22a905f57e002ae542bf1f7942518
Aravind <gunturaravind@gmail.com>
2026-05-08 18:47:43 +0530

    updated buildspec

--- terraform/modules/codebuild/main.tf
+++ terraform/modules/codebuild/main.tf
@@ -57,6 +57,20 @@ resource "aws_iam_role_policy" "codebuild_policy" {
       {
         Effect = "Allow"
         Action = [
+          "s3:GetObject",
+          "s3:GetObjectVersion",
+          "s3:PutObject",
+          "s3:GetBucketVersioning",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::${var.project_name}-${var.environment}-pipeline-artifacts",
+          "arn:aws:s3:::${var.project_name}-${var.environment}-pipeline-artifacts/*"
+        ]
+      },
+      {
+        Effect = "Allow"
+        Action = [
           "sts:GetCallerIdentity"
         ]
         Resource = "*"
